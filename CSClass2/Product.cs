6fbe4b4f61e7d7416fbda3c9d6d0f04b016f10c0
SRin23 <serin4723@naver.com>
2022-09-04 16:32:52 +0900

    #6 인터페이스 실습 : IComparable

---
+++ CSClass2/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            return this.Name.CompareTo(other.Name);
+            //return this.Price - other.Price;
+            //return this.Price.CompareTo(other.Price);
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " : " + this.Price + " 원";
+        }
+    }
+}
